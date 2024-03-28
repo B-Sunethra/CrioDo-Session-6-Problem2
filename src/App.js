9eaadc8d3bfb22733fb2a252e04a2d756fb143e3
B-Sunethra <120642526+B-Sunethra@users.noreply.github.com>
2024-03-28 03:25:55 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Button from "./Button";
+import SearchBar from "./SearchBar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+    </div>
+  );
+}
