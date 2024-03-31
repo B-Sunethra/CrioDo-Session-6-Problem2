4f098795a17f5a4b3353d0f7fcbb1ee8985d9dce
B-Sunethra <120642526+B-Sunethra@users.noreply.github.com>
2024-03-30 22:47:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Button from "./Button";
 import SearchBar from "./SearchBar";
+import HeroSection from "./Components/HeroSection";
 
 export default function App() {
   return (
     <div className="App">
       <SearchBar />
+      <HeroSection />
     </div>
   );
 }
