f2f030d81d75418eb2cb731dbc7faa49d2e6b0ed
B-Sunethra <120642526+B-Sunethra@users.noreply.github.com>
2024-03-30 22:45:14 -0400

    Create HeroSection.js

---
+++ src/Components/HeroSection.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./HeroSection.css"; // make sure to create this CSS file in your project
+import logo from "./vibrating-headphone.png";
+
+const HeroSection = () => {
+  return (
+    <div className="hero-container">
+      <div className="content">
+        <p className="sub-title">100 Thousand Songs, ad-free</p>
+        <p className="main-title">Over thousands podcast episodes</p>
+      </div>
+      <img src={logo} alt="Headphones" className="headphones-image" />
+    </div>
+  );
+};
+
+export default HeroSection;
