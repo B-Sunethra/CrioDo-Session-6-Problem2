3e9740661c907c6b9a9cd9043e634af686f17104
B-Sunethra <120642526+B-Sunethra@users.noreply.github.com>
2024-04-07 23:48:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import SearchBar from "./SearchBar";
-import HeroSection from "./Components/HeroSection";
 import AlbumLayout from './Components/AlbumLayout';
+import SongsLayout from './Components/SongsLayout';
 
 function App() {
   return (
     <div>
-      <SearchBar />
-      <HeroSection />
       <AlbumLayout headerLeft="Top Albums" headerRight="Collapse" fetchAPI="https://qtify-backend-labs.crio.do/albums/top"/>
       <AlbumLayout headerLeft="New Albums" headerRight="Collapse" fetchAPI="https://qtify-backend-labs.crio.do/albums/new"/>
+      <SongsLayout/>
     </div>
   );
 }
